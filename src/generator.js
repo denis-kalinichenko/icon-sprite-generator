28585ab511b7dbeec633869c79fc69c04db5a6b4
Denis Kalinichenko <denis@kalinichenko.me>
2018-02-09 10:18:02 +0100

    multiple instances support

--- src/generator.js
+++ src/generator.js
@@ -2,13 +2,14 @@ const { path } = require("@uix/icons");
 
 const matchFiles = require("./components/matchFiles");
 const processFiles = require("./components/processFiles");
-const spriter = require("./components/spriter");
+const SVGSpriter = require("./components/spriter");
 const compileSprite = require("./components/spriterCompiler");
 
 module.exports = function iconSpriteGenerator(input = path) {
     return matchFiles(input)
         .then(processFiles)
         .then(collection => {
+            const spriter = SVGSpriter();
             collection.forEach(file => {
                 spriter.add(file);
             });
